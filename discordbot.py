7f3e01a5ac000a8b293872aa1de74ac682b94159
Rai-Zamine <61692498+Rai-Zamine@users.noreply.github.com>
2020-03-03 02:44:10 +0900

    MEE6をもっと楽しく

--- discordbot.py
+++ discordbot.py
@@ -11,6 +11,30 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
+   async def on_message(message):
+    if message.author.id == 159985870458322944: # MEE6からのメッセージかどうかを判別
+        if message.content.startswith("!levelup"):
+            await message.delete() # メッセージを消去
+
+            level = int(message.content.split()[-2]) # メッセージを分解
+            t_name = message.content.split()[-1] # メッセージを分解
+            target = discord.utils.get(message.server.members, mention=t_name) # レベルアップしたユーザーのIDを取得
+
+            replys = f"{t_name}さん、が{str(level)}レべになりました。" # レベルアップメッセージ
+            await message.channel.send(replys)
+
+            if level == 1: # レベル1になった時の処理
+                levelrole1 = discord.utils.get(message.server.roles, name="初心者")
+                await target.add_roles(levelrole1)
+
+            elif level == '5': # レベル5になった時の処理
+                levelrole1 = discord.utils.get(message.server.roles, name="初心者")
+                levelrole5 = discord.utils.get(message.server.roles, name="アマチュア")
+                await target.add_roles(levelrole5)
+                await target.remove_roles(levelrole1)
+
+client.run(token) # Botのトークン
 
 
 @bot.command()
